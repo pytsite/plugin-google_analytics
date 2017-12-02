274b9acb3355d17665cbcdd7e0baaebea516fa34
Alexander Shepetko <a@shepetko.com>
2017-12-02 10:32:25 +0200

    Release 1.1

--- _eh.py
+++ _eh.py
@@ -1,7 +1,7 @@
 """PytSite Google Analytics Plugin Event Handlers
 """
-from pytsite import tpl as _tpl, lang as _lang, router as _router
-from plugins import assetman as _assetman, settings as _settings, auth as _auth
+from pytsite import tpl as _tpl, lang as _lang, router as _router, reg as _reg
+from plugins import assetman as _assetman, auth as _auth
 
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
@@ -11,7 +11,7 @@ __license__ = 'MIT'
 def router_dispatch():
     """'pytsite.router.dispatch' handler.
     """
-    t_id = _settings.get('google_analytics.tracking_id')
+    t_id = _reg.get('google_analytics.tracking_id')
     if not t_id and _auth.get_current_user().has_permission('google_analytics.settings.manage'):
         _router.session().add_warning_message(_lang.t('google_analytics@plugin_setup_required_warning'))
     else:
